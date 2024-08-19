db6c58e0f4e8b2962a26c1d7c56c7e1fffc8fdbe
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-08-20 00:22:16 +0900

    0820 깃허브 연습

---
+++ OSW Share Project/src/OSW/osw_1.java
@@ -0,0 +1,14 @@
+package OSW;
+
+public class osw_1 {
+
+	public static void main(String[] args) {
+		System.out.println("하이");
+		System.out.println("하이");
+		System.out.println("하이");
+		System.out.println("하이");
+		System.out.println("하이");
+
+	}
+
+}
