93d40eb2abc26f8506ff9572e9bb0166c7f55db6
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-09-10 23:04:24 +0900

    Add files via upload

---
+++ SelectHomeWork.java
@@ -0,0 +1,31 @@
+
+
+//1442 : 선택 정렬
+
+
+
+/**#include <stdio.h>
+int a[10001];
+int n, i, j, temp, min;
+int main() {
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+        
+        (정답)
+    for (i=1; i<n; i++) {
+        min=i;
+        
+        
+        for (j=i+1; j<=n; j++) {
+if(a[min] > a[j]) 
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+    for (i=1; i<=n; i++)
+        printf("%d\n", a[i]);
+    return 0;
+}
+*/
